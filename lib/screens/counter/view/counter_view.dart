6c27e9a6b4a6e59929742752fea3fcaae7a18b56
sebastian <scastellanos931@gmail.com>
2024-08-20 16:46:18 -0500

    first commit

---
+++ lib/screens/counter/view/counter_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+
+class CounterViewModel extends ChangeNotifier {
+int _count = 0;
+int get count => _count;
+void increment() {
+_count++;
+notifyListeners();
+}
+void decrement() {
+_count--;
+notifyListeners();
+}
+}
